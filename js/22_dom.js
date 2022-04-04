2ccb3ee4db653f63208fa9bce2e9267f5081d605
Rajani Kanth Katukojwala <rajkatukojwa@factset.com>
2022-04-04 16:09:57 +0530

    dommmm

--- js/22_dom.js
+++ js/22_dom.js
@@ -7,6 +7,14 @@ Info on Documents: https://javascript.info/document
 With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
 document object is a property of the window object.
 If you check for the members of document all are on html.
+
+Similar to CSS the selectors in JS to manipulate a Dom Object are: id (for unique element), class (for a group of elements), element name (for all elements with the name)
 */
 
-console.log("dom..");
+console.log("Accessing the Dom elements from javascript.")
+console.log(document.getElementsByClassName("divByClassExample"));
+console.log(document.getElementsByTagName("divBynameExample"));
+
+console.log(document.getElementById("divByIdExample"));
+
+console.log(document.getElementById("divByIdExample"))
